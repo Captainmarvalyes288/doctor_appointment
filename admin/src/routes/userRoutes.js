e06a4bbdf8e23e11e6459e52a36f9380a558e811
madhuri-cha <madhurichavan15122003@gmail.com>
2025-04-09 12:49:17 +0530

    db changes

--- admin/src/routes/userRoutes.js
+++ admin/src/routes/userRoutes.js
@@ -4,5 +4,7 @@ import { login } from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/bookAppointment', bookAppointment);
+router.get('/getAllDoctors', getAllDoctors);
 
 export default router; 
